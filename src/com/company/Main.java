4bd277f96e522389d2ed74251976a5616c44325e
lzyyy <951489398@qq.com>
2020-11-10 21:25:14 +0800

    2020年11月10日

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,38 @@ import com.company.offer.*;
 import javax.management.StandardEmitterMBean;
 import java.lang.reflect.Array;
 import java.util.*;
+import java.util.concurrent.RunnableFuture;
 import java.util.regex.Pattern;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
+
         /*TreeNode treeNode = new TreeNode(1);
         treeNode.left = new TreeNode(2);
         treeNode.left.left = new TreeNode(3);
         treeNode.left.left.left = new TreeNode(4);*/
-        boolean[] x = new boolean[1];
-        System.out.println(Arrays.deepToString(new merge().merge(new int[][]{{1, 3}, {2, 6}, {8, 10}, {13, 18}})));
+        System.out.println(AccountType.FIXED);
+    }
+    enum AccountType
+    {
+        SAVING, FIXED, CURRENT;
+        private AccountType()
+        {
+            System.out.println("It is a account type");
+        }
+    }
+
+
+    private int maxProfit(int[] prices) {
+        if (prices.length <= 1){
+            return 0;
+        }
+        int[][] dp = new int[prices.length][2];
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < prices.length; i++){
+            dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0] - prices[i]);
+        }
+        return dp[prices.length-1][0];
     }
 }
---
+++ src/com/company/ladderLength.java
@@ -0,0 +1,90 @@
+package com.company;
+
+import java.util.*;
+
+public class ladderLength {
+    /**
+     * 127. 单词接龙
+     * 给定两个单词（beginWord 和 endWord）和一个字典，找到从 beginWord 到 endWord 的最短转换序列的长度。转换需遵循如下规则：
+     *
+     * 每次转换只能改变一个字母。
+     * 转换过程中的中间单词必须是字典中的单词。
+     * 说明:
+     *
+     * 如果不存在这样的转换序列，返回 0。
+     * 所有单词具有相同的长度。
+     * 所有单词只由小写字母组成。
+     * 字典中不存在重复的单词。
+     * 你可以假设 beginWord 和 endWord 是非空的，且二者不相同。
+     * 示例 1:
+     *
+     * 输入:
+     * beginWord = "hit",
+     * endWord = "cog",
+     * wordList = ["hot","dot","dog","lot","log","cog"]
+     *
+     * 输出: 5
+     *
+     * 解释: 一个最短转换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+     *      返回它的长度 5。
+     * 示例 2:
+     *
+     * 输入:
+     * beginWord = "hit"
+     * endWord = "cog"
+     * wordList = ["hot","dot","dog","lot","log"]
+     *
+     * 输出: 0
+     *
+     * 解释: endWord "cog" 不在字典中，所以无法进行转换。
+     * @param beginWord
+     * @param endWord
+     * @param wordList
+     * @return
+     */
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+        if (!wordList.contains(endWord)){
+            return 0;
+        }
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        visited.add(beginWord);
+        int ladderLength = 0;
+        while (queue.size() > 0){
+            int size = queue.size();
+            ++ladderLength;
+            for (int i = 0; i < size; ++i){
+                String start = queue.poll();
+                for (String s : wordList){
+                    if (visited.contains(s)) {
+                        continue;
+                    }
+                    if (!cmp(s,start)) {
+                        continue;
+                    }
+                    if (s.equals(endWord)) {
+                        return ladderLength + 1;
+                    }
+                    visited.add(s);
+                    queue.offer(s);
+                }
+            }
+
+        }
+
+        return 0;
+    }
+    private boolean cmp(String a, String b){
+        int count = 0;
+        for (int i =0 ; i < a.length(); i++){
+            if (a.charAt(i) != b.charAt(i)){
+                ++count;
+                if (count>1)
+                    return false;
+            }
+        }
+        return count == 1;
+    }
+}
